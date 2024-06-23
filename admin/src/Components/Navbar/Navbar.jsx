6a301b77ff2588e3af6a8728dc4797f8c04cd05c
Karthikeya <bairojukarthikeya@gmail.com>
2024-06-23 23:52:23 +0530

    Updated admin navbar

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Navbar.css'
-import navlogo from '../Assets/nav-logo.svg'
+import navlogo from '../Assets/logo.svg'
 import navprofileIcon from '../Assets/nav-profile.svg'
 
 const Navbar = () => {
